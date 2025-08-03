46aac91e474cfe327cc9a3d522bdaa3f21a9088d
Tony Shangkuan <tonyshangkuan@gmail.com>
2025-08-03 21:23:05 +0800

    complete rest api security

--- src/main/java/com/example/demo/security/DemoSecurityConfig.java
+++ src/main/java/com/example/demo/security/DemoSecurityConfig.java
@@ -5,15 +5,35 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
+    // add support for JDBC no more hardcode
+    /*@Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }*/
+
     @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        //define query to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery("select user_id, pw, active from members where user_id=?");
+
+        //define query to retrieve the authorities/roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select user_id, role from roles where user_id=?");
+
+        return jdbcUserDetailsManager;
+    }
+
+    /*@Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
         UserDetails john = User.builder()
                 .username("john")
@@ -34,7 +54,7 @@ public class DemoSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
-    }
+    }*/
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -42,8 +62,9 @@ public class DemoSecurityConfig {
                 .requestMatchers(HttpMethod.GET, "/api/employee", "/magic-api/employees").hasRole("EMPLOYEE")
                 .requestMatchers(HttpMethod.GET, "/api/employee/**", "/magic-api/employees/**").hasRole("EMPLOYEE")
                 .requestMatchers(HttpMethod.POST, "/api/employee", "/magic-api/employees").hasRole("MANAGER")
-                .requestMatchers(HttpMethod.PUT, "/api/employee", "/magic-api/employees").hasRole("MANAGER")
-                .requestMatchers(HttpMethod.GET, "/api/employee/**", "/magic-api/employees/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/api/employee", "/magic-api/employees/**").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.PATCH, "/api/employee/**", "/magic-api/employees/**").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.DELETE, "/api/employee/**", "/magic-api/employees/**").hasRole("ADMIN")
         );
 
         //use HTTP basic authentication
